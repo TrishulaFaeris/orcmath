c29ba7c284f81602cc80e2d24a297d726fcf8c43
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-01-02 11:31:09 -0500

    Added a few thingz

---
+++ OrcMathGUI/src/kevinPackage/CustomButton.java
@@ -0,0 +1,43 @@
+package kevinPackage;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import com.itextpdf.awt.geom.Rectangle;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class CustomButton extends Button {
+
+	private String string1;
+	private String string2;
+	private Color color;
+	
+	public CustomButton(int x, int y) {
+		super(x, y, 50, 50, "", null);
+		string1 = "Hello";
+		string2 = "Bye";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void drawButton(Graphics2D g, boolean hover) {
+		g.setColor(Color.BLACK);
+		g.drawString(string1, 40, 80);
+		g.drawString(string2, 40, 120);
+		g.fillRect(0, 0, 50, 50);
+	}
+	public void updateString1(String string) {
+		
+	}
+	public void updateString2(String string) {
+		
+	}
+	public void setIconColor(Color color) {
+		
+	}
+
+}
